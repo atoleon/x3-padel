d3be7f4ae55769673fe39060ff99aa4601e0c96b
atoleon <atoleon@gmail.com>
2026-02-23 19:03:25 +0000

    custom scores

--- src/components/MatchInfoCard.tsx
+++ src/components/MatchInfoCard.tsx
@@ -20,9 +20,7 @@ export interface MatchData {
 
 export default function MatchInfoCard({ match }: { match: MatchData }) {
   const sets = [match.result["1"], match.result["2"], match.result["3"]];
-  const hasScores = sets.some(
-    (s) => s && s[0] !== "-" && s[1] !== "-"
-  );
+  console.log(sets);
 
   return (
     <div className="match-info-card">
@@ -44,22 +42,19 @@ export default function MatchInfoCard({ match }: { match: MatchData }) {
             <span>{match.team2.player_b}</span>
           </div>
         </div>
-
-        {hasScores && (
-          <div className="match-info-scores">
-            {sets.map((set, i) => {
-              if (!set || (set[0] === "-" && set[1] === "-")) return null;
-              const s1 = Number(set[0]);
-              const s2 = Number(set[1]);
-              return (
-                <div key={i} className="match-info-set">
-                  <span className={s1 > s2 ? "set-winner" : ""}>{set[0]}</span>
-                  <span className={s2 > s1 ? "set-winner" : ""}>{set[1]}</span>
-                </div>
-              );
-            })}
+        <div className="match-info-scores">
+          <div className="match-info-set">
+            <span>{sets[0][0]}</span>
+            <span>{sets[1][0]}</span>
+            <span>{sets[2][0]}</span>
           </div>
-        )}
+          <div className="match-info-divider" />
+          <div className="match-info-set">
+            <span>{sets[0][1]}</span>
+            <span>{sets[1][1]}</span>
+            <span>{sets[2][1]}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
